f470d6a897efcbb940d7b2758d6247ecd1062507
chemoie <1217318381@qq.com>
2020-06-28 00:03:11 +0800

    update1

--- FileuploadPro/FileuploadPro/app/views/modules.py
+++ FileuploadPro/FileuploadPro/app/views/modules.py
@@ -83,3 +83,37 @@ def upload1():
             response = jsonify(file_dict)
             response.headers['ifjson'] = 1
             return response
+@modules_bp.route('/upload2/',methods=['POST'],endpoint='upload2')
+def upload2():
+    # 获取请求头token，与upload0相同
+    rec_token=request.headers['token']
+    if request.method=='POST':
+        try:
+            #token判定
+            if rec_token!=Token:
+                file_dict = {
+                    'status': 401,
+                    'msg': '请求需要验证'
+                }
+                response = jsonify(file_dict)
+                response.headers['ifjson'] = 1
+                return response
+            file_obj0=request.files.get('data0')
+            file_name = file_obj0.filename
+            file_con = file_obj0.read().decode(encoding='utf8')
+            #调用函数生成文件
+            fname=getfile.get_file(file_name,file_con)
+            #返回文件
+            response=make_response(send_from_directory('static',fname,as_attachment=True))
+            response.headers['Content-Disposition']="p_w_upload; filename=test.txt"
+            response.headers['ifjson']=0
+            return response
+        except Exception as e:
+            print(e)
+            file_dict = {
+                'status': 404,
+                'msg':'失败，请重传'
+            }
+            response = jsonify(file_dict)
+            response.headers['ifjson'] = 1
+            return response
